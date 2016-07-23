5abbee2e3950fe8f3baabae0a8f7dd796317b647
Lucy Kang <HaeYeon.Kang@MyGeorgian.ca>
2016-07-22 22:27:39 -0400

    Add project files.

---
+++ RestaurantSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantSite.MvcApplication" Language="C#" %>
---
+++ RestaurantSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantSite.Startup))]
+namespace RestaurantSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
